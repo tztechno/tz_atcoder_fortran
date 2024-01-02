f572e1bfb34b82a1ef8fbfec1ea85b7fa9769942
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-02 12:17:01 +0900

    Create not.f90

---
+++ not.f90
@@ -0,0 +1,15 @@
+
+//ABC178_A Not
+
+program Main
+  implicit none
+  integer :: N
+  read(*,*) N
+  
+  if (N==0) then
+        write(*,*) 1
+    else
+        write(*,*) 0  
+    endif            
+
+end program Main
