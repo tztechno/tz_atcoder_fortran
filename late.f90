995df16949031e0511f2ae51b29b8acd3de543f3
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-06 14:12:36 +0900

    Create late.f90

---
+++ late.f90
@@ -0,0 +1,14 @@
+//ABC177_A late
+
+program xxx
+
+  real :: D, T, S
+  read(*,*) D, T, S
+
+  if (D <= T * S) then
+      write(*,*) 'Yes'
+  else 
+      write(*,*) 'No'
+  end if
+
+end program xxx
