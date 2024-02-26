c5042d486e0938972368ad579d44ad1140e51309
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-02-26 15:33:24 +0900

    Create multiplication.f90

---
+++ multiplication.f90
@@ -0,0 +1,33 @@
+//abc169_a multiplication.f90
+################################
+################################
+program main
+	implicit none
+    integer::a,b
+    read(*,*) a,b
+    write(*,*) a*b
+    stop
+end program main
+################################
+program main
+  implicit none
+  integer :: a, b
+  read *, a, b
+  print "(i0)", a * b
+end program main
+################################
+program main
+    implicit none
+    integer::a,b
+    read(*,*)a,b
+    write(*,*)a*b
+    stop
+    contains
+end program main
+################################
+program xxx
+  integer ::a,b
+  read(*,*) a,b
+  write(*,*) a*b
+end program xxx
+################################
