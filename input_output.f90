9cc29d2b4ddbcd7357aedf0725e0680c02a0aa21
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-04-25 13:19:37 +0900

    Update input_output.f90

--- input_output.f90
+++ input_output.f90
@@ -4,26 +4,20 @@ program main
     integer, allocatable :: A(:)
     
     ! Prompt the user for input
-    print *, "Enter the number of elements (N):"
     read(*, *) N
-    
-    ! Prompt the user for K
-    print *, "Enter the value of K:"
     read(*, *) K
     
     ! Allocate memory for the list
     allocate(A(N))
     
     ! Prompt the user for the list of integers
-    print *, "Enter the list of integers separated by spaces:"
     do i = 1, N
         read(*, *) A(i)
     end do
     
     ! Output N, K, and the list for demonstration purposes
-    print *, "Value of N:", N
-    print *, "Value of K:", K
-    print *, "The list of integers:"
+    print *, N
+    print *, K
     do i = 1, N
         print *, A(i)
     end do
